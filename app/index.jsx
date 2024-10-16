dfbd35ed58433fb5057d8a9b24b55aac765c29af
PikriNtr <fikrihaikal2101@gmail.com>
2024-10-17 00:03:31 +0800

    added first page for introduction

--- app/index.jsx
+++ app/index.jsx
@@ -7,17 +7,30 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function App() {
   return (
     <SafeAreaView className="bg-white h-full">
-      <ScrollView contentContainerStyle={{height: '100%'}} className="bg-bg">
-        <View className="w-full justify-center items-center h-full px-4">
-          <Text className="relative font-psemibold text-3xl mb-[-50] top-0 left-0 text-white">PikriNtr</Text>
-          <View className="bg-primary w-52 h-52 rounded-full relative top-52 right-40"></View>
-          <View className="bg-accent w-52 h-52 rounded-full relative -bottom-72 left-40"></View>
-          <View className="relative mb-[230]">
-            <Text className="text-3xl text-white font-pmedium text-center">Ini cuma beta test dari app yg gw bikin</Text>
-            <Link href="/home" className="text-yellow text-1xl text-center font-pregular">Click ini buat ke menu sebelah</Link>
-          </View>
+      <View className='bg-indigo-950 h-full'>
+        <View className='my-80 border border-transparent bg-yellow rounded-full w-64 h-12 left-14 items-center justify-center'>
+          <Link href='/home' className='text-2xl text-center font-pmedium'>click here to start</Link>
         </View>
-      </ScrollView>
+      </View>
+      
+      
     </SafeAreaView>
   );
 }
+
+
+
+
+
+
+
+
+{/* <ScrollView contentContainerStyle={{height: '100%'}} className="bg-bg">
+        <View className="w-full justify-center items-center h-full px-4">
+          <Text className="relative font-psemibold text-3xl mb-[-50] top-0 left-0 text-white">PikriNtr</Text>
+          <View className="relative mb-[230]">
+            <Text className="text-3xl text-white font-pmedium text-center">Pihon</Text>
+            <Link href="/home" className="text-yellow text-1xl text-center font-pregular">Click ini buat ke menu sebelah</Link>
+          </View>
+        </View>
+      </ScrollView> */}
